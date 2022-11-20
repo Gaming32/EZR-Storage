c2223e9c15533c8baf617c187d38fed0cc21fecf
stygigoth <nyxwilliams@protonmail.com>
2022-11-20 14:21:58 -0500

    Initial commit

---
+++ src/main/java/io/github/stygigoth/ezrstorage/Main.java
@@ -0,0 +1,10 @@
+package io.github.stygigoth.ezrstorage;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
