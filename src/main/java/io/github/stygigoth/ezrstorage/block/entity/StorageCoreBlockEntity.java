2307dda69b2774c2a1115140bce59d6f8abe29f9
stygigoth <nyxwilliams@protonmail.com>
2022-11-22 15:29:32 -0500

    Optimization

--- src/main/java/io/github/stygigoth/ezrstorage/block/entity/StorageCoreBlockEntity.java
+++ src/main/java/io/github/stygigoth/ezrstorage/block/entity/StorageCoreBlockEntity.java
@@ -17,19 +17,26 @@ import java.util.Map;
 public class StorageCoreBlockEntity extends BlockEntity implements ImplementedInventory {
     private final Map<BlockPos, RefBlockEntity> network = new HashMap<>();
     private final DefaultedList<List<ItemStack>> inventory = DefaultedList.of();
+    private final DefaultedList<ItemStack> items = DefaultedList.of();
 
     public StorageCoreBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.STORAGE_CORE_BLOCK_ENTITY, pos, state);
-        scan(world);
+        scanBlocks(world);
         if (world != null) {
             network.forEach(((blockPos, refBlockEntity) -> {
                 if (refBlockEntity instanceof StorageBoxBlockEntity)
                     inventory.add(((StorageBoxBlockEntity) refBlockEntity).inventory);
             }));
         }
+        scanInventories();
     }
 
     public void scan(WorldAccess world) {
+        scanBlocks(world);
+        scanInventories();
+    }
+
+    private void scanBlocks(WorldAccess world) {
         for (RefBlockEntity be : network.values()) {
             be.core = null;
         }
@@ -62,9 +69,12 @@ public class StorageCoreBlockEntity extends BlockEntity implements ImplementedIn
         }
     }
 
+    private void scanInventories() {
+        items.clear();
+        inventory.forEach(items::addAll);
+    }
+
     public DefaultedList<ItemStack> getItems() {
-        DefaultedList<ItemStack> inv = DefaultedList.of();
-        inventory.forEach(inv::addAll);
-        return inv;
+        return items;
     }
 }
